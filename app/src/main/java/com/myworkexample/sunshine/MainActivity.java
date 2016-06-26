bf5fb454ca5280a1572af984ae546f4de622e94c
jatinbalodhi96 <jatinbalodhi@gmail.com>
2016-06-26 10:46:14 +0530

    Added SharedIntent

--- app/src/main/java/com/myworkexample/sunshine/MainActivity.java
+++ app/src/main/java/com/myworkexample/sunshine/MainActivity.java
@@ -1,14 +1,20 @@
 package com.myworkexample.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String LOG_TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferrefLocationInMap(){
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String location = sharedPrefs.getString(
+          getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
     }
 
 }
